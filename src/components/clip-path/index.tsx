bc50f6dc0f9e3349af5aae895797a372ca217b1f
GreattitJY <great.tit.jaejae@gmail.com>
2024-12-26 15:27:07 +0900

    wip: clip path content needs to be added

--- src/components/clip-path/index.tsx
+++ src/components/clip-path/index.tsx
@@ -1,10 +1,17 @@
 import "./styles.css";
 import Example from "./example";
 import Showcase from "../_common/showcase";
+import Badge from "../_common/badge";
 
 export default function ClipPath() {
   return (
-    <div className="">
+    <div className="flex flex-col gap-2.5">
+      <h2 className="text-2xl">Clip Path</h2>
+      <p>Clip path를 활용한 애니메이션입니다.</p>
+      <div className="ml-auto space-x-2.5">
+        <Badge>css</Badge>
+        <Badge>key frames</Badge>
+      </div>
       <Showcase>
         <Example />
       </Showcase>
