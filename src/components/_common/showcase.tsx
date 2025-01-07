01a2a18af90b4d5473d44dd91d5c0961953267cf
GreattitJY <great.tit.jaejae@gmail.com>
2025-01-07 15:40:39 +0900

    add: reset button

--- src/components/_common/showcase.tsx
+++ src/components/_common/showcase.tsx
@@ -1,19 +1,32 @@
 import { cn } from "@/lib/utils";
-import { PropsWithChildren } from "react";
+import { RotateCcw } from "lucide-react";
+import { PropsWithChildren, useState } from "react";
 
 interface ShowcaseProps extends PropsWithChildren {
+  isReset?: boolean;
   className?: string;
 }
 
-export default function Showcase({ className, children }: ShowcaseProps) {
+export default function Showcase({ className, isReset = false, children }: ShowcaseProps) {
+  const [key, setKey] = useState(0);
+
   return (
     <div
       className={cn(
-        "border-2 rounded-lg border-dashed flex items-center justify-center py-10 bg-gray-50",
+        "relative border-2 rounded-lg border-dashed flex items-center justify-center py-10 bg-gray-50",
         className
       )}
     >
-      {children}
+      {isReset && (
+        <button
+          className="absolute top-2.5 right-2.5 hover:text-gray-600 active:-rotate-45 transition-all"
+          type="button"
+          onClick={() => setKey((prev) => prev + 1)}
+        >
+          <RotateCcw className="size-5" />
+        </button>
+      )}
+      <div key={key}>{children}</div>
     </div>
   );
 }
--- src/components/text-reveal/index.tsx
+++ src/components/text-reveal/index.tsx
@@ -7,7 +7,7 @@ export default function TextReveal({}: TextRevealProps) {
   return (
     <section className="flex flex-col gap-8">
       <h2 className="text-title">Text Reveal</h2>
-      <Showcase>
+      <Showcase isReset>
         <Example />
       </Showcase>
     </section>
